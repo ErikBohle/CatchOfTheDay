47bb36d7e265dfdee508d158b0f82782a73b76cb
erikbohle <55728992+ErikBohle@users.noreply.github.com>
2020-07-24 01:05:23 +0200

    testing commits and deploying

---
+++ catch-of-the-day/build/precache-manifest.46bba4db9b59224fdc5018a58f5afc70.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "67d3576a0b7989bea32533bae9b21761",
+    "url": "/index.html"
+  },
+  {
+    "revision": "03936e6d895360031282",
+    "url": "/static/css/main.79dec417.chunk.css"
+  },
+  {
+    "revision": "0862d6407796d2de3e90",
+    "url": "/static/js/2.016878d1.chunk.js"
+  },
+  {
+    "revision": "ad951958ed7766763ed3e6c72917393c",
+    "url": "/static/js/2.016878d1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "03936e6d895360031282",
+    "url": "/static/js/main.d64cfaa2.chunk.js"
+  },
+  {
+    "revision": "91973092a8adabe4af5a",
+    "url": "/static/js/runtime-main.76b6e058.js"
+  },
+  {
+    "revision": "d2f8799a13e6db2022d9a7de22776705",
+    "url": "/static/media/anchor.d2f8799a.svg"
+  },
+  {
+    "revision": "1461f8e9d2084c70c00830348efe1766",
+    "url": "/static/media/blanch_caps_inline-webfont.1461f8e9.woff"
+  },
+  {
+    "revision": "73a576a8254aeafebcdc66fe9fd41ac8",
+    "url": "/static/media/blanch_caps_inline-webfont.73a576a8.eot"
+  },
+  {
+    "revision": "8345e1b3362c1d640f0f11b5aae1d5bc",
+    "url": "/static/media/blanch_caps_inline-webfont.8345e1b3.ttf"
+  },
+  {
+    "revision": "c175fe49ca6c599199a81c6791490344",
+    "url": "/static/media/blanch_caps_inline-webfont.c175fe49.svg"
+  },
+  {
+    "revision": "47e26dee9b053b540478750bdf7a87d5",
+    "url": "/static/media/haymaker-webfont.47e26dee.svg"
+  },
+  {
+    "revision": "4c3fb584d3f63821357c8a9cff97f360",
+    "url": "/static/media/haymaker-webfont.4c3fb584.eot"
+  },
+  {
+    "revision": "751e56274d3ea1c651ea824f902bb987",
+    "url": "/static/media/haymaker-webfont.751e5627.woff"
+  },
+  {
+    "revision": "e9535f20ca175cc3fdfe2816f7c47168",
+    "url": "/static/media/haymaker-webfont.e9535f20.ttf"
+  }
+]);
