bc5fdbeb89611c7775670c1069fe74293085c215
erikbohle <55728992+ErikBohle@users.noreply.github.com>
2020-08-15 00:02:38 +0200

    Added note about API key

--- catch-of-the-day/src/base.js
+++ catch-of-the-day/src/base.js
@@ -1,6 +1,8 @@
 import Rebase from 're-base';
 import firebase from 'firebase';
 
+
+// API isn't hidden currently, so isn't secure
 const firebaseApp = firebase.initializeApp({
   apiKey: 'AIzaSyCljo197J2V2Y3uRvHsE9SCrN-VUaNd8qI',
   authDomain: 'ebwesbos.firebaseapp.com',
